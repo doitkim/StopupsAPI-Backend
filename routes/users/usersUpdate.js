9241b4c0ff3eaa543857faec35187abdcd3b4023
chellengerKim <skkim27214@gmail.com>
2023-01-05 15:34:34 +0900

    회원 탈퇴 조건 상세화

--- routes/users/usersUpdate.js
+++ routes/users/usersUpdate.js
@@ -1,19 +1,25 @@
 const express = require("express"); // 익스프레스 라이브러리 불러오기
 const User = require("../../models/user"); // User 모델 불러오기
+const { Op } = require("sequelize");
 const router = express.Router(); // 익스프레스의 라우터 불러오기
 
 router
   // /user/ get 요청 오면 유저 테이블 전체 조회 후 JSON 형식으로 응답
   .route("/update")
   .post(async (req, res, next) => {
+    // console.log(req.body);
     try {
-      // console.log(req.body);
       const user = await User.update(
         {
           userPw: req.body.eUserPw,
         },
         {
-          where: { userPhoneNumber: req.body.eUserPhoneNumber },
+          where: {
+            [Op.and]: {
+              userPhoneNumber: req.body.eUserPhoneNumber,
+              userId: req.body.eUserId,
+            },
+          },
         }
       );
       res.status(201).json(user); // 유저 추가 후 JSON 갱신
